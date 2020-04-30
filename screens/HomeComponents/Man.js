de81f77757a5d97999999fa4d074ed5711b42add
Nadeem Mukhtar <nadeem.ucp@gmail.com>
2020-05-01 02:48:03 +0500

    'WashComponent'

--- screens/HomeComponents/Man.js
+++ screens/HomeComponents/Man.js
@@ -1,21 +1,43 @@
 import React, {Component} from 'react';
-import {TouchableOpacity,StatusBar ,StyleSheet,Keyboard,SafeAreaView ,TextInput ,Text, View,ImageBackground,TouchableWithoutFeedback, KeyboardAvoidingView, Image} from 'react-native';
-
-
+import {TouchableOpacity,StatusBar,FlatList, RefreshControl ,StyleSheet,TextInput ,Text, View,ImageBackground,TouchableWithoutFeedback, KeyboardAvoidingView, Image} from 'react-native';
+import WashComponent from '../components/WashComponent'
 
 export default class Home extends Component {
     constructor(props)
 {
     super(props);
     this.state={
-        email:'',
-        pass: '',
-    };
+      data:[
+        {id:'0',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'1',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'2',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'3',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'4',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'5',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'6',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'7',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'8',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'9',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'10',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+        {id:'11',image:'../images/1.png',price:'6.00',title:'T-Shirt',quantity:0},
+      ]};
 }
     render(){
   return (
     <View style={{flex:1}}>
-        <StatusBar barStyle = "white" backgroundColor = '#fc6203'/>
+      <FlatList 
+          data={this.state.data}
+          contentContainerStyle={{paddingRight: 20}}
+          refreshControl={
+            <RefreshControl 
+              refreshing={this.state.isLoading}
+              onRefresh={()=>this.componentDidMount()}/>
+          }
+          renderItem={({ item, index }) => (
+            <WashComponent index={item.id} image ={item.image} price={item.price} title={item.title} quantity={item.quantity} navigation={this.props.navigation}  />
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
     </View>
   );
 }
--- screens/components/WashComponent.js
+++ screens/components/WashComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {TouchableOpacity,StatusBar ,StyleSheet,Keyboard,SafeAreaView ,TextInput ,Text, View,ImageBackground,TouchableWithoutFeedback, KeyboardAvoidingView, Image} from 'react-native';
-
+import { Icon } from "react-native-elements";
 
 
 export default class WashComponent extends Component {
@@ -8,16 +8,52 @@ export default class WashComponent extends Component {
 {
     super(props);
     this.state={
-        email:'',
-        pass: '',
+        quantityTotal:this.props.quantity
     };
+}
+qauntities = (text) => {
+  console.log(this.state.quantityTotal)
+        if(text=='Plus'){
+         return this.setState({quantityTotal:this.state.quantityTotal+1})
+        }
+        if(text=='Minus'){
+          if(this.state.quantityTotal>0){
+           return this.setState({quantityTotal:this.state.quantityTotal-1})
+          }
+          
+        }
 }
     render(){
   return (
-    <View style={{flex:1}}>
-        
-        
-    </View>
+    <View style={{height:90,justifyContent:"center",backgroundColor:'white',marginTop:5}}>
+          <View style={{flexDirection:"row"}}>
+              <View style={{flex:.5}}>
+                  <View style={{flexDirection:'row'}}>
+                    <View style={{paddingLeft:10}}><Image source={require('../../images/1.png')} style={{width:70,height:70}} /></View>
+                      <View style={{marginLeft:10}}>
+                        <View style={{marginTop:10}}><Text style={{fontSize:22,fontWeight:"bold"}}>{this.props.title}</Text></View>
+                        <View style={{flexDirection:"row",marginTop:5}}><Text style={{fontSize:15,color:'#fc6203'}}>{this.state.quantityTotal} </Text><Text style={{fontSize:15,color:'#fc6203'}}>X </Text><Text style={{fontSize:15,color:'#fc6203'}}>${this.props.price}</Text></View>
+                      </View>
+                  </View>
+              </View>
+              <View style={{flex:.5,justifyContent:"center"}}>
+                  <View style={{flexDirection:'row',justifyContent:"flex-end",paddingRight:20}}>
+                    <View style={{flexDirection:"row",marginTop:5}}>
+                      <TouchableOpacity onPress={()=>this.qauntities('Minus')}>
+                        <Icon type="antdesign" name="minuscircleo" size={22} />
+                      </TouchableOpacity>
+                      <View style={{width:40,alignItems:"center"}}>
+                        <Text style={{fontSize:18}}>{this.state.quantityTotal}</Text>
+                      </View>
+                      <TouchableOpacity onPress={()=>this.qauntities('Plus')}>
+                        <Icon type="antdesign" name="pluscircleo" size={22} />
+                      </TouchableOpacity>
+                    </View>
+                      
+                  </View>
+              </View>
+          </View>
+      </View>
   );
 }
 }
